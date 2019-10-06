8a2f9239fdde02ff4d95c2976a3a482a468aa34f
misoA <alth7512@gmail.com>
2019-10-07 03:23:09 +0900

    Updates

---
+++ precache-manifest.c1468cef6b39a413d2341bc5bb2858dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fd7c5c5ac417e222b895c5828eb410",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "336c9e67d9e4766443c3",
+    "url": "/react_movie_app/static/css/main.4c150388.chunk.css"
+  },
+  {
+    "revision": "c2a8125e2ea267e88c9b",
+    "url": "/react_movie_app/static/js/2.980e377c.chunk.js"
+  },
+  {
+    "revision": "336c9e67d9e4766443c3",
+    "url": "/react_movie_app/static/js/main.17be8ab5.chunk.js"
+  },
+  {
+    "revision": "90604c7c154f05b59466",
+    "url": "/react_movie_app/static/js/runtime-main.c3d2af94.js"
+  }
+]);
